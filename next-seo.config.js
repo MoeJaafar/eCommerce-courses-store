c1271ef9a9d2a12258e69713bb6a763681aae152
Mohammad Jaafar <m.jaafar@innopolis.university>
2023-07-25 06:22:34 +0300

    added some features in SEO config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,5 +1,6 @@
 export default {
   title: "Market Place",
+  url: "https://e-commerce-courses-store.vercel.app",
   description:
     "e-Commerce market to buy and sell courses powered with crypto payment",
   openGraph: {
@@ -7,5 +8,6 @@ export default {
     locale: "en_US",
     url: "https://e-commerce-courses-store.vercel.app",
     site_name: "Market Place",
+    coverImage: "https://searchlogovector.com/wp-content/uploads/2018/06/marketplace-org-logo-vector.png"
   },
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { NextSeo } from "next-seo";
+import SEO from "../next-seo.config";
 
 export default function Document() {
   return (
+    
+    
     <Html lang="en">
+      <NextSeo
+        title={SEO.title}
+        description={SEO.description}
+        openGraph={{
+          images: [
+            {
+              url: SEO.openGraph.coverImage,
+            },
+          ],
+        }}
+        canonical={SEO.url}
+      />
       <Head />
       <body>
         <Main />
